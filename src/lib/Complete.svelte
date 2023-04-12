3a734d474e2a93c755d187a5be1f860c17ae6118
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-12 11:46:18 +0500

    Add styles

--- src/lib/Complete.svelte
+++ src/lib/Complete.svelte
@@ -3,8 +3,35 @@
 </script>
 
 <div class="complete">
-	<img src={iconComplete} alt="" />
+	<img class="icon" src={iconComplete} alt="" />
 	<h2 class="complete__header">Thank you!</h2>
 	<p class="complete__description">We've added your card details</p>
 	<slot />
 </div>
+
+<style>
+	.complete {
+		display: grid;
+		justify-items: center;
+	}
+
+	.icon {
+		margin-block-end: 2rem;
+	}
+
+	.complete__header {
+		margin-block-end: 1rem;
+
+		font-weight: 500;
+		line-height: 1;
+		text-transform: uppercase;
+		color: var(--color-neutral-800);
+		letter-spacing: 2px;
+	}
+
+	.complete__description {
+		margin-block-end: 2.5rem;
+		color: var(--color-neutral-400);
+		font-size: var(--font-size-500);
+	}
+</style>
